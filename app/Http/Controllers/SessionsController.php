210d7da102715b457cac62fe6c244792ba02e790
ShamazinGG <nikbelikita1@yandex.ru>
2021-08-14 01:38:43 +0300

    [+] Comments Component

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -18,16 +18,16 @@ class SessionsController extends Controller
             'password'=> 'required'
         ]);
 
-        if (auth()->attempt($attributes)) {
-            session()->regenerate();
+        if (!auth()->attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified.',
+            ]);
 
-            return redirect('/')->with('success', 'Welcome Back!');
-        };
+        }
 
-        throw ValidationException::withMessages([
-           'email' => 'Your provided credentials could not be verified.',
-        ]);
+        session()->regenerate();
 
+        return redirect('/')->with('success', 'Welcome Back!');
     }
 
     public function destroy()
