bd2faa1bdce606c16bd19ddb02fc5577ee33a259
ShamazinGG <nikbelikita1@yandex.ru>
2021-08-23 13:28:34 +0300

    [+] Make Comments dynamic

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,19 +1,21 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div style="flex-shrink: 0;">
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">Shamez</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
 
             <p class="text-xs">Posted
-                <time>8 months ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
 
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            {{ $comment->body }}
         </p>
     </div>
 </article>
