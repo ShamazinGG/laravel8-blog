d9b379c441f1499cd7db624c7143c7d7895842f7
ShamazinGG <nikbelikita1@yandex.ru>
2021-08-31 01:05:42 +0300

    [+] Install mailchimp

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,20 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+   $mailchimp = new MailchimpMarketing\ApiClient();
+
+   $mailchimp->setConfig([
+      'apiKey' => config('services.mailchimp.key'),
+      'server' => 'us5'
+   ]);
+
+   $response = $mailchimp->lists->addListMember('9be8215304', [
+       'email_address' => 'testtest@gmail.com',
+       'status' => 'subscribed'
+   ]);
+   ddd($response);
+});
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
