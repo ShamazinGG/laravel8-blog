180c544a1502783bac869e2a56d309cb3e9d99a9
ShamazinGG <nikbelikita1@yandex.ru>
2021-08-31 23:34:56 +0300

    [+] Newsletter Service

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,20 @@
 <?php
 
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Services\Newsletter;
 use Illuminate\Support\Facades\Route;
-
-Route::get('ping', function () {
-   $mailchimp = new MailchimpMarketing\ApiClient();
-
-   $mailchimp->setConfig([
-      'apiKey' => config('services.mailchimp.key'),
-      'server' => 'us5'
-   ]);
-
-   $response = $mailchimp->lists->addListMember('9be8215304', [
-       'email_address' => 'testtest@gmail.com',
-       'status' => 'subscribed'
-   ]);
-   ddd($response);
-});
+use Illuminate\Validation\ValidationException;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [CommentController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
