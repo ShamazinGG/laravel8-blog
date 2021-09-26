0d505e12522acccf4458031fe81506b53d4eb071
ShamazinGG <nikbelikita1@yandex.ru>
2021-09-27 02:20:25 +0300

    [+] AdminPost CRUD

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -5,11 +5,9 @@
     <x-form.label name="{{ $name }}" />
 
     <input class="border border-gray-200 p-2 w-full rounded"
-           name="{{ $name }} }}"
-           id="{{ $name }} }}"
-           value="{{ old($name) }}"
-           required
-           {{ $attributes }}
+           name="{{ $name }}"
+           id="{{ $name }}"
+           {{ $attributes(['value' => old($name)]) }}
     >
 
     <x-form.error name="{{ $name }}"/>
